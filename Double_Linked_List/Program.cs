1232d6095b7a949bbaadc7d33b982cd0528b3ce8
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-11-14 16:46:31 +0700

    Add project files.

---
+++ Double_Linked_List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Linked_List
+{
+    class node
+    {
+        public int noMhs;
+        public string name;
+        public node next;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
