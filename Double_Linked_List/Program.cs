672b615c559f4acd08072a6b8499e6ce847e289f
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-11-14 16:54:06 +0700

    membuat class doublelinkedlist

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -8,10 +8,28 @@ namespace Double_Linked_List
 {
     class node
     {
+        /*node class represents the node of doubly linked list.
+         * it consists of the information part and linked to.
+         * its succeeding and preceeding.
+         * it terms of next and proveious*/
         public int noMhs;
         public string name;
+        //point to the succeding node
         public node next;
+        //point to the precceeding node
+        public node prev;
 
+    }
+
+    class doublelinkedlist
+    {
+        node START;
+
+        //Construktor
+        public doublelinkedlist()
+        {
+            START = null;
+        }
     }
     class Program
     {
